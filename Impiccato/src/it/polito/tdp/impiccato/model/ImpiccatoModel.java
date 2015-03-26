13689b75d4a72503283e06e8bb40e642ec84a136
fcorno <fulvio.corno@polito.it>
2015-03-26 15:52:48 +0100

    versione con connessione al database (estrazione automatica della parola segreta)

--- Impiccato/src/it/polito/tdp/impiccato/model/ImpiccatoModel.java
+++ Impiccato/src/it/polito/tdp/impiccato/model/ImpiccatoModel.java
@@ -1,5 +1,10 @@
 package it.polito.tdp.impiccato.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,7 +13,7 @@ public class ImpiccatoModel {
 	private String segreto;
 	private int errori;
 	private final int MAX_ERRORI = 8;
-	private String maschera ;
+	private String maschera;
 
 	private Set<String> provate;
 
@@ -35,69 +40,128 @@ public class ImpiccatoModel {
 
 	/**
 	 * Inizia una nuova partita
-	 * @param segreto La parola da indovinare
+	 * 
+	 * @param segreto
+	 *            La parola da indovinare
 	 */
 	public void startGame(String segreto) {
 		this.segreto = segreto.toUpperCase();
-		
+
 		errori = 0;
 		provate.clear();
-		
-		maschera = "" ;
-		for(int i=0; i<segreto.length(); i++)
-			maschera = maschera+"_" ;
+
+		maschera = "";
+		for (int i = 0; i < segreto.length(); i++)
+			maschera = maschera + "_";
 	}
 
 	/**
 	 * Controlla se la lettera specificata fa parte della parola. Lettere non
 	 * esistenti oppure lettere ripetute sono valutate come errori.
 	 * 
-	 * @param t La lettera da provare
-	 * @return true se la lettera esiste, false se non esiste oppure se era già stata detta
+	 * @param t
+	 *            La lettera da provare
+	 * @return true se la lettera esiste, false se non esiste oppure se era già
+	 *         stata detta
 	 */
 	public boolean tryLetter(String t) {
-		t = t.toUpperCase() ;
+		t = t.toUpperCase();
 		// già detta?
-		if(provate.contains(t)) {
-			errori ++ ;
-			return false ;
+		if (provate.contains(t)) {
+			errori++;
+			return false;
 		}
-		
-		provate.add(t) ;
-		
+
+		provate.add(t);
+
 		// presente?
-		if(segreto.contains(t)) {
+		if (segreto.contains(t)) {
 			// presente
-			
-			for(int i=0; i<segreto.length(); i++) {
-				if(segreto.charAt(i) == t.charAt(0)) {
-					maschera = maschera.substring(0, i) + t + maschera.substring(i+1) ;
+
+			for (int i = 0; i < segreto.length(); i++) {
+				if (segreto.charAt(i) == t.charAt(0)) {
+					maschera = maschera.substring(0, i) + t
+							+ maschera.substring(i + 1);
 				}
 			}
-			
-			return true ;
+
+			return true;
 		} else {
 			// assente
-			errori++ ;
-			return false ;
+			errori++;
+			return false;
 		}
 	}
-	
+
 	/**
 	 * Determina se la parola è stata indovinata
 	 * 
-	 * @return true se è indovinata, false se vi sono ancora lettere da indovinare
+	 * @return true se è indovinata, false se vi sono ancora lettere da
+	 *         indovinare
 	 */
 	public boolean isWinner() {
-		return ( ! maschera.contains("_")) ;
+		return (!maschera.contains("_"));
 	}
-	
+
 	/**
 	 * Determina se hai perso la partita
 	 * 
 	 * @return true se hai perso, false se sei ancora in gioco
 	 */
 	public boolean isLoser() {
-		return ( errori >= MAX_ERRORI ) ;
+		return (errori >= MAX_ERRORI);
+	}
+
+	/**
+	 * Genera una nuova parola segreta, estraendola casualmente dal Dizionario
+	 * memorizzato nel database
+	 * 
+	 * @return una parola casuale
+	 */
+	public String nuovoSegreto() {
+		String url = "jdbc:mysql://localhost/dizionario?user=root";
+
+		try {
+			Connection conn = DriverManager.getConnection(url);
+
+			String query1 = "select count(id) as numero from parola";
+
+			Statement st1 = conn.createStatement();
+			ResultSet res1 = st1.executeQuery(query1);
+
+			res1.first();
+			int numeroParole = res1.getInt("numero");
+
+			res1.close();
+
+			int indiceParola = (int) (Math.random() * numeroParole);
+
+			String query2 = "select nome from parola limit " + indiceParola
+					+ ", 1";
+
+			/*
+			String query2 = String.format(
+					"select nome from parola limit %d, 1", 
+					indiceParola) ; */
+			
+			Statement st2 = conn.createStatement() ;
+			ResultSet res2 = st2.executeQuery(query2) ;
+			
+			res2.first() ;
+			String segreto = res2.getString("nome") ;
+			
+			res2.close();
+			
+			conn.close();
+			
+			return segreto.toUpperCase() ;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null ;
+
 	}
 }
